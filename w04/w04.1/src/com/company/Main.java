913985c9939d1462fbf911a38848e54b654ff16f
Kaden <kadenpayne@gmail.com>
2021-10-09 17:44:51 -0700

    w04

---
+++ w04/w04.1/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("This program converts a hexadecimal digit into a four digit binary number.\n Enter a hexadecimal number: ");
+
+    }
+}
