27750067e74783668ac8f44d28f9f8fe1d314c95
Kaden <kadenpayne@gmail.com>
2021-10-14 18:25:10 -0700

    finished w05.1

--- w05/w05.1/src/com/company/w05dot1.java
+++ w05/w05.1/src/com/company/w05dot1.java
@@ -4,5 +4,20 @@ public class w05dot1 {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("This program will output a table showing pounds per kilogram.\n");
+
+        System.out.print("""
+                kilograms\t\tpounds
+                ---------\t\t------
+                """);
+        int intKilo;
+        double dblPound;
+
+        for (intKilo = 1; intKilo <= 15; intKilo += 2) {
+            dblPound = (2.2 * intKilo);
+            System.out.printf(intKilo + "\t\t\t\t" + "%-3.1f\n", dblPound);
+        }
+
+        System.out.println("\nGoodbye.");
     }
 }
