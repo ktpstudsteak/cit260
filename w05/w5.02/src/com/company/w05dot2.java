a059b3aed594a7a836b36da239fcaf629268c65a
Kaden <kadenpayne@gmail.com>
2021-10-14 18:45:11 -0700

    w5.02 started

---
+++ w05/w5.02/src/com/company/w05dot2.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class w05dot2 {
+
+    public static void main(String[] args) {
+	// Purpose
+        System.out.println("This program displays all of the numbers from 100 to 1000\n" +
+                            "that are divisible by both 5 and 6.");
+        int f;
+        for (int i = 100; i <= 1000; i++) {
+            f = i;
+            for (y = 1; y <= 10; y++){
+
+                if(f % 5 == 0 && f % 6 == 0 ){
+
+                }
+            }
+
+
+        }
+
+    }
+}
