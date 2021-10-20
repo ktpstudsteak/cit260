193be7aa75b1927e41a45620218ea24eb29f76ad
Kaden <kadenpayne@gmail.com>
2021-10-19 17:48:45 -0700

    ratio done, currency calc started

--- w06/misc/src/com/company/Main.java
+++ w06/misc/src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    public static double getDouble(double dbl1, double dbl2){
+        return (dbl1 / dbl2);
+    }
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("This program returns a ratio of 2 numbers using a method\n");
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter 1st Number: ");
+        double dbl1 = in.nextDouble();
+
+        System.out.print("Enter 2nd Number: ");
+        double dbl2 = in.nextDouble();
+
+        double dblResult = getDouble(dbl1, dbl2);
+
+        System.out.printf("The Ratio is %.2f" , dblResult);
     }
 }
