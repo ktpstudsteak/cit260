e7a6474755618a73f90c438d0ce98279302b7d48
Kaden <kadenpayne@gmail.com>
2021-10-21 17:41:07 -0700

    began real work on 6.2

--- w06/6.2/src/com/company/w06dot2.java
+++ w06/6.2/src/com/company/w06dot2.java
@@ -2,7 +2,28 @@ package com.company;
 
 public class w06dot2 {
 
+    /**
+     * The celsiusToFahrenheit Method
+     * Purpose: Convert Celsius to Fahrenheit
+     * @param tempCelsius The principle investment amount
+     */
+    public static double celsiusToFahrenheit(double tempCelsius){
+        //Add code
+    }
+
+    /**
+     * The fahrenheitToCelsius Method
+     * Purpose: Convert Fahrenheit to Celsius
+     * @param tempFahrenheit The principle investment amount
+     */
+    public static double fahrenheitToCelsius(double tempFahrenheit){
+
+    }
     public static void main(String[] args) {
 	// write your code here
+
+        //Maybe try for loops using the
+        //you don't need to input anything. just calculate based on the left hand column on each side
+        System.out.println("This program will print out a table of conversions from Celsius to Fahrenheit and vice versa.\n");
     }
 }
