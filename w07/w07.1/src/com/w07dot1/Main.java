9913f96237b04dbd6320eb3db14f7ad1f70dbcd8
Kaden <kadenpayne@gmail.com>
2021-10-30 23:04:27 -0700

    began 7.1

---
+++ w07/w07.1/src/com/w07dot1/Main.java
@@ -0,0 +1,29 @@
+package com.w07dot1;
+
+public class Main {
+
+    /**
+     * The mean Method
+     * Purpose: Determine a future value of an investment
+     * @param x The monthly interest rate
+     */
+    public static double mean(double[ ] x){
+        System.out.printf("");
+        double dblX = 2.0;
+        return dblX;
+    }
+
+    /**
+     * The deviation Method
+     * Purpose: Determine a future value of an investment
+     * @param x The monthly interest rate
+     */
+    public static double deviation(double[ ] x){
+        System.out.printf("");
+        double dblX = 2.0;
+        return dblX;
+    }
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
