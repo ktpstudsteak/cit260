79ee2033b4146b94392ca04dfef3df63ff160dd2
Kaden <kadenpayne@gmail.com>
2021-11-05 17:53:13 -0700

    methods should be all done now

--- w08/src/com/company/rectangle.java
+++ w08/src/com/company/rectangle.java
@@ -12,6 +12,7 @@ public class rectangle {
         height = 1;
     }
 
+    //Allows them not to be just one? delete if it doesn't work.
     rectangle(double pWidth, double pHeight){
         width = pWidth;
         height = pHeight;
@@ -36,7 +37,45 @@ public class rectangle {
      * @param pHeight the height of the rectangle object
      * @return double
      */
-    public static double getPerimeter(double pWidth, double pHeight){
+    public double getPerimeter(double pWidth, double pHeight){
         return (pWidth * 2) + (pHeight * 2);
     }
+
+    /**
+     * the setHeight method
+     * Purpose: Gets the height from input and sets it as the class var height
+     * @param pHeight the height of the rectangle object being given by the user
+     */
+    public void setHeight(double pHeight) {
+        this.height = pHeight;
+    }
+
+    /**
+     * the getHeight method
+     * Purpose: Returns the height AFTER it has been set by setHeight()
+     * @return double
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * the setWidth method
+     * Purpose: Gets the width from input and sets it as the class var height
+     * @param pWidth the width of the rectangle object being given by the user
+     */
+    public void setWidth(double pWidth) {
+        this.width = pWidth;
+    }
+
+    /**
+     * the getWidth method
+     * Purpose: Returns the height AFTER it has been set by setWidth()
+     * @return double
+     */
+    public double getWidth() {
+        return width;
+    }
+
+
 }
--- w08/src/com/company/w08dot1.java
+++ w08/src/com/company/w08dot1.java
@@ -3,12 +3,7 @@ package com.company;
 public class w08dot1 {
 
     public static void main(String[] args) {
-        /**
-         * the setTitle method
-         * Purpose” to set the title of a book object
-         * @param  the title to set, as a string
-         * @return void
-         */
+
     }
 }
 
