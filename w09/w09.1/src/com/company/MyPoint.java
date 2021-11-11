0843e59b1b7b9b5afba352a0ff444041cf031aab
Kaden <kadenpayne@gmail.com>
2021-11-10 20:49:10 -0700

    not sure what this is but like ok

---
+++ w09/w09.1/src/com/company/MyPoint.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class MyPoint {
+    //Params
+    public static double x;
+    public static double y;
+
+    //Getters and setters
+
+
+    /** Gets the X-Coordinate
+     * @return The x value
+     */
+    public static double getX() {
+        return x;
+    }
+
+    /**
+     * Sets the X-Coordinate
+     * @param x The X value
+     */
+    public static void setX(double x) {
+        MyPoint.x = x;
+    }
+
+    /**
+     * Gets the Y-Coordinate
+     * @return  y The Y value
+     */
+    public static double getY() {
+        return y;
+    }
+
+    /**
+     * Sets the Y-Coordinate
+     * @param y The Y value
+     */
+    public static void setY(double y) {
+        MyPoint.y = y;
+    }
+}
